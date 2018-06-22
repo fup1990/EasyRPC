e42994b48d134293e720e41736ae95c316dfd9fe
fup1990 <fup1990@163.com>
2018-06-22 11:30:28 +0800

    handler

---
+++ remoting/src/main/java/com/gome/fup/easy/rpc/remoting/handler/ServerHandler.java
@@ -0,0 +1,15 @@
+package com.gome.fup.easy.rpc.remoting.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * Created by fupeng-ds on 2018/6/20.
+ */
+public class ServerHandler extends SimpleChannelInboundHandler {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
