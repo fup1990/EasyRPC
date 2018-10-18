778752d83142fa85d795fb90d7bacfac77feae67
fupeng <fup1990@163.com>
2018-10-18 21:52:01 +0800

    主从同步

--- nameserver/src/main/java/com/gome/fup/easy/rpc/nameserver/ha/HaService.java
+++ nameserver/src/main/java/com/gome/fup/easy/rpc/nameserver/ha/HaService.java
@@ -4,5 +4,5 @@ package com.gome.fup.easy.rpc.nameserver.ha;
  * 主从同步服务
  * Created by fupeng-ds on 2018/7/11.
  */
-public class HaService {
+public interface HaService {
 }
---
+++ nameserver/src/main/java/com/gome/fup/easy/rpc/nameserver/ha/HaServiceImpl.java
@@ -0,0 +1,7 @@
+package com.gome.fup.easy.rpc.nameserver.ha;
+
+/**
+ * Created by fupeng on 2018/10/18.
+ */
+public class HaServiceImpl implements HaService {
+}
